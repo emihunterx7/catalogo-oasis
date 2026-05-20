70fefe40e1e2cf48f3c5986d0b8f790e113df32d
emihunterx7 <106021522+emihunterx7@users.noreply.github.com>
2026-05-20 11:16:20 -0300

    ER

--- catalogo.py
+++ catalogo.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 BANNER_OFERTAS = [
     {
        "titulo": "🔥 ¡Nuevos Ingresos!",
-        "descripcion": "Llegaron las últimas novedades exclusivas. ¡No te quedes sin la tuya!",
+        "descripcion": "MAPLE DE HUEVO BLANCO $4800 ",
         "imagen": "https://raw.githubusercontent.com/emihunterx7/catalogo-oasis/11f042053004aa1188c218acd5a5ffc7c76805cc/oferta%20huevos.png",
         "badge": "OFERTA DE LA SEMANA"
     },
